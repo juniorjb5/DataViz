cf933f9c5f42af0bc8920fe032afaa8fb92118f0
Junior Joaqui B <juniorjb5@hotmail.com>
2024-02-20 16:19:40 -0500

    Act.20.02.2024_1

--- CorrerSubirPagina.R
+++ CorrerSubirPagina.R
@@ -2,15 +2,9 @@
 
 
 
-blogdown::build_site()
-blogdown::serve_site()
-blogdown::stop_server()
-
-
-
 git add .
 
-git commit -m "Act.08.04.2023_1"
+git commit -m "Act.16.04.2023_1"
 
 git push origin main
 
